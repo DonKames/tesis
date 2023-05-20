d9951b549b6700e45d028026f8b63173e20d6b80
Camilo Santander <camilo.santander1991@gmail.com>
2023-05-20 00:25:06 -0400

    commit

--- src/modules/locations/components/LocationsScreen.jsx
+++ src/modules/locations/components/LocationsScreen.jsx
@@ -20,6 +20,7 @@ import {
 import { getRegions } from '../APIs/apiRegions';
 import { getCountries } from '../APIs/apiCountries';
 import { createBranch } from '../APIs/apiBranches';
+import { AddWarehouseModal } from './AddWarehouseModal';
 
 export const LocationsScreen = () => {
     const dispatch = useDispatch();
@@ -149,80 +150,7 @@ export const LocationsScreen = () => {
                     </Table>
                 </Col>
             </Row>
-            <Modal
-                show={showAddBranchModal}
-                onHide={handleClose}
-            >
-                <Modal.Header closeButton>
-                    <Modal.Title>Agregar Sucursal</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <Form
-                        onSubmit={handleFormSubmit}
-                        ref={formRef}
-                    >
-                        <Row>
-                            <Col>
-                                <Form.Group className='mb-3'>
-                                    <Form.Label>Nombre Sucursal</Form.Label>
-                                    <Form.Control
-                                        type='text'
-                                        placeholder='Ingrese el nombre de la Sucursal'
-                                        name='branchName'
-                                        value={branchName}
-                                        onChange={handleInputChange}
-                                    />
-                                </Form.Group>
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col>
-                                <Form.Group className='mb-3'>
-                                    <Form.Label>País</Form.Label>
-                                    <Select
-                                        isSearchable
-                                        name='country'
-                                        options={countryOptions}
-                                        onChange={handleCountryChange}
-                                        placeholder='Seleccione País'
-                                    />
-                                </Form.Group>
-                            </Col>
-                            <Col>
-                                <Form.Group className='mb-3'>
-                                    <Form.Label>Región</Form.Label>
-                                    <Select
-                                        isSearchable
-                                        name='region'
-                                        options={regionsOptions}
-                                        onChange={handleRegionChange}
-                                        placeholder='Seleccione Región'
-                                    />
-                                </Form.Group>
-                            </Col>
-                        </Row>
-                        <Form.Group>
-                            <Form.Label>Dirección</Form.Label>
-                            <Form.Control
-                                type='text'
-                                placeholder='Ingrese la Dirección'
-                                name='address'
-                                value={address}
-                                onChange={handleInputChange}
-                            />
-                        </Form.Group>
-                    </Form>
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button
-                        type='button'
-                        variant='primary'
-                        onClick={handleFormSubmit}
-                    >
-                        Guardar Sucursal
-                    </Button>
-                </Modal.Footer>
-            </Modal>
+            <AddWarehouseModal />
         </Container>
     );
 };
