fb4dad9e59b0364989611c792be0dab4e95443e7
Camilo Santander <camilo.santander1991@gmail.com>
2023-05-31 17:20:22 -0400

    Working

--- src/modules/products/components/AddProductsScreen.jsx
+++ src/modules/products/components/AddProductsScreen.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button, Card, Col, Container, Form, Row } from 'react-bootstrap';
 import { useForm } from '../../../hooks/useForm';
 import { createProduct, getProductBySku } from '../APIs/apiProducts';
@@ -9,6 +9,8 @@ import { locationsSetBranches } from '../../locations/slice/locationsSlice';
 
 export const AddProductsScreen = () => {
     const dispatch = useDispatch();
+
+    const [searchedProduct, setSearchedProduct] = useState(null);
 
     const { branches } = useSelector((state) => state.locations);
 
@@ -22,42 +24,47 @@ export const AddProductsScreen = () => {
         fetchData();
     }, [getBranches, dispatch]);
 
-    // const branchOptions = branches.map((branch) => ({
-    //     value: branch.branch_id,
-    //     label: branch.name,
-    // }));
+    const branchOptions = branches.map((branch) => ({
+        value: branch.branch_id,
+        label: branch.name,
+    }));
 
-    const [formValues, handleInputChange] = useForm({
-        branchId: '',
-        name: 'camilo@hotmail.com',
+    const [formSkuValues, handleSkuInputChange] = useForm({
+        name: 'Chaqueta Roja',
         description: 'La descripción del producto',
         price: '10000',
-        // Qty: '150',
         sku: 'FA654',
         lote: '4444',
         order: '123',
     });
 
-    const { name, description, price, qty, sku, lote, order } = formValues;
+    const { name, description, price, sku, lote, order } = formSkuValues;
 
-    const handleAddProduct = () => {
-        console.log('Agregando producto');
+    const [formProductValues, handleProductInputChange] = useForm({
+        fkSku: '',
+        epc: '',
+    });
+
+    const { epc, fkSku } = formProductValues;
+
+    const handleAddSku = () => {
+        console.log('Agregando Sku');
         const searchProduct = getProductBySku(sku);
 
         if (!searchProduct) {
             console.log('El producto no existe');
-            createProduct(formValues);
+            createProduct(formSkuValues);
         }
     };
 
-    // const handleBranchChange = (selectedOption) => {
-    //     handleInputChange({
-    //         target: {
-    //             name: 'branchId',
-    //             value: selectedOption ? selectedOption.value : '',
-    //         },
-    //     });
-    // };
+    const handleBranchChange = (selectedOption) => {
+        handleProductInputChange({
+            target: {
+                name: 'branchId',
+                value: selectedOption ? selectedOption.value : '',
+            },
+        });
+    };
 
     return (
         <Container>
@@ -66,28 +73,18 @@ export const AddProductsScreen = () => {
                     <h1>Agregar Productos</h1>
                     <Card className='mb-3'>
                         <Card.Body>
-                            <Card.Title>Agregar Producto</Card.Title>
+                            <Card.Title>Agregar SKU</Card.Title>
                             <Form>
                                 <Form.Group>
                                     <Form.Label>SKU del producto</Form.Label>
                                     <Form.Control
                                         type='text'
-                                        placeholder='Ingrese el SKU del producto'
+                                        placeholder='Ingrese el SKU para el producto'
                                         name='sku'
                                         value={sku}
-                                        onChange={handleInputChange}
+                                        onChange={handleSkuInputChange}
                                     />
                                 </Form.Group>
-                                {/* <Form.Group>
-                                    <Form.Label>Sucursal</Form.Label>
-                                    <Select
-                                        isSearchable
-                                        placeholder='Seleccione la Sucursal'
-                                        name='fk_branch_id'
-                                        options={branchOptions}
-                                        onChange={handleBranchChange}
-                                    />
-                                </Form.Group> */}
                                 <Form.Group>
                                     <Form.Label>Nombre del producto</Form.Label>
                                     <Form.Control
@@ -95,7 +92,7 @@ export const AddProductsScreen = () => {
                                         placeholder='Ingrese el nombre del producto'
                                         name='name'
                                         value={name}
-                                        onChange={handleInputChange}
+                                        onChange={handleSkuInputChange}
                                     />
                                 </Form.Group>
                                 <Form.Group>
@@ -107,7 +104,7 @@ export const AddProductsScreen = () => {
                                         placeholder='Ingrese la descripción del producto'
                                         name='description'
                                         value={description}
-                                        onChange={handleInputChange}
+                                        onChange={handleSkuInputChange}
                                     />
                                 </Form.Group>
                                 <Form.Group>
@@ -117,19 +114,9 @@ export const AddProductsScreen = () => {
                                         placeholder='Ingrese el precio del producto'
                                         name='price'
                                         value={price}
-                                        onChange={handleInputChange}
+                                        onChange={handleSkuInputChange}
                                     />
                                 </Form.Group>
-                                {/* <Form.Group>
-                                    <Form.Label>Stock del producto</Form.Label>
-                                    <Form.Control
-                                        type='number'
-                                        placeholder='Ingrese el stock del producto'
-                                        name='qty'
-                                        value={qty}
-                                        onChange={handleInputChange}
-                                    />
-                                </Form.Group> */}
                                 <Form.Group>
                                     <Form.Label>Lote del producto</Form.Label>
                                     <Form.Control
@@ -137,7 +124,7 @@ export const AddProductsScreen = () => {
                                         placeholder='Ingrese el lote del producto'
                                         name='lote'
                                         value={lote}
-                                        onChange={handleInputChange}
+                                        onChange={handleSkuInputChange}
                                     />
                                 </Form.Group>
                                 <Form.Group>
@@ -147,7 +134,7 @@ export const AddProductsScreen = () => {
                                         placeholder='Ingrese el orden del producto'
                                         name='order'
                                         value={order}
-                                        onChange={handleInputChange}
+                                        onChange={handleSkuInputChange}
                                     />
                                 </Form.Group>
                             </Form>
@@ -155,7 +142,68 @@ export const AddProductsScreen = () => {
                         <Card.Footer>
                             <Button
                                 className='btn btn-primary'
-                                onClick={handleAddProduct}
+                                onClick={handleAddSku}
+                            >
+                                Guardar Producto
+                            </Button>
+                        </Card.Footer>
+                    </Card>
+                </Col>
+                <Col>
+                    <h1>Agregar Productos</h1>
+                    <Card className='mb-3'>
+                        <Card.Body>
+                            <Card.Title>Agregar Producto</Card.Title>
+                            <Form>
+                                <Form.Group>
+                                    <Form.Label>Código SKU</Form.Label>
+                                    <Form.Control
+                                        type='text'
+                                        placeholder='Ingrese el Sku del producto'
+                                        name='name'
+                                        value={fkSku}
+                                        onChange={handleProductInputChange}
+                                    />
+
+                                    {searchedProduct &&
+                                        (searchedProduct ? (
+                                            <span style={{ color: 'green' }}>
+                                                El producto existe
+                                            </span>
+                                        ) : (
+                                            <span style={{ color: 'red' }}>
+                                                El producto no existe
+                                            </span>
+                                        ))}
+                                </Form.Group>
+                                <Form.Group>
+                                    <Form.Label>Sucursal</Form.Label>
+                                    <Select
+                                        isSearchable
+                                        placeholder='Seleccione la Sucursal'
+                                        name='fk_branch_id'
+                                        options={branchOptions}
+                                        onChange={handleBranchChange}
+                                    />
+                                </Form.Group>
+                                <Form.Group>
+                                    <Form.Label>
+                                        Código Etiqueta (EPC)
+                                    </Form.Label>
+                                    <Form.Control
+                                        type='text'
+                                        placeholder='Ingrese el epc para el producto'
+                                        name='name'
+                                        value={epc}
+                                        onChange={handleProductInputChange}
+                                    />
+                                </Form.Group>
+                            </Form>
+                        </Card.Body>
+                        <Card.Footer>
+                            <Button
+                                className='btn btn-primary'
+                                onClick={handleAddSku}
                             >
                                 Guardar Producto
                             </Button>
