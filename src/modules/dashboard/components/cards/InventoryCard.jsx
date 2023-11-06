406402646e0e53c79af8ae31e0d2e7be4c54b160
DevKam <camilo.santander1991@gmail.com>
2023-11-06 17:09:06 -0300

    Add ListGroupItem and timestamp utility functions

--- src/modules/dashboard/components/cards/InventoryCard.jsx
+++ src/modules/dashboard/components/cards/InventoryCard.jsx
@@ -1,5 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import { Card, Button, ListGroup, Row, Col } from 'react-bootstrap';
+import {
+    Card,
+    Button,
+    ListGroup,
+    Row,
+    Col,
+    ListGroupItem,
+} from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import {
     getProductsCountByWarehouse,
@@ -9,6 +16,10 @@ import { productsSetProductQty } from '../../../products/slice/productsSlice';
 import { CustomBarChart } from '../../../../shared/ui/components/charts/CustomBarChart';
 import { getLastAddedProducts } from '../../../movements/APIs/movementAPI';
 import { movementsSetData } from '../../../movements/movementSlice';
+import {
+    timestampDate,
+    timestampTime,
+} from '../../../../shared/utils/stringUtils';
 
 export const InventoryCard = () => {
     // {
@@ -53,9 +64,9 @@ export const InventoryCard = () => {
                 }
 
                 if (!lastAdded.length) {
-                    const { data } = await getLastAddedProducts(5);
+                    const { data } = await getLastAddedProducts(6);
 
-                    console.log(data);
+                    // console.log(data);
 
                     dispatch(movementsSetData({ lastAdded: data }));
                 }
@@ -91,27 +102,96 @@ export const InventoryCard = () => {
 
                 {/* Sección de Ítems Recientemente Añadidos */}
                 <ListGroup.Item>
-                    <h6>Ítems Recientemente Añadidos</h6>
-                    <div className="recent-items-container">
+                    <Col xs={12} lg={4}>
+                        <h4>Ítems Recientemente Añadidos</h4>
                         {lastAdded.map((m) => (
-                            <Card key={m.id}>
+                            <Card key={m.id} className="mb-2">
+                                <Card.Header>
+                                    <Card.Title>
+                                        <strong>{m.name}</strong>
+                                    </Card.Title>
+                                </Card.Header>
+
+                                {/*
+                                    // * Para ver como queda con lineas separadoras
+                                */}
+                                {/* <ListGroup>
+                                    <ListGroupItem>
+                                        <Row className="mb-2">
+                                            <Col xs={4}>
+                                                <Card.Text>
+                                                    Descripción
+                                                </Card.Text>
+                                            </Col>
+                                            <Col xs={1}>
+                                                <Card.Text>:</Card.Text>
+                                            </Col>
+                                            <Col xs={7}>
+                                                <Card.Text>
+                                                    {m.description}
+                                                </Card.Text>
+                                            </Col>
+                                        </Row>
+                                    </ListGroupItem>
+                                </ListGroup> */}
                                 <Card.Body>
-                                    <Card.Title>{m.productName}</Card.Title>
-                                    <Card.Text>
-                                        Cantidad: {m.quantity}
-                                    </Card.Text>
+                                    <Row className="mb-2">
+                                        <Col xs={4}>
+                                            <Card.Text>Descripción</Card.Text>
+                                        </Col>
+                                        <Col xs={1}>
+                                            <Card.Text>:</Card.Text>
+                                        </Col>
+                                        <Col xs={7}>
+                                            <Card.Text>
+                                                {m.description}
+                                            </Card.Text>
+                                        </Col>
+                                    </Row>
+                                    <Row className="mb-2">
+                                        <Col xs={4}>
+                                            <Card.Text>Usuario</Card.Text>
+                                        </Col>
+                                        <Col xs={1}>
+                                            <Card.Text>:</Card.Text>
+                                        </Col>
+                                        <Col xs={7}>
+                                            <Card.Text>
+                                                {m.userFirstName +
+                                                    ' ' +
+                                                    m.userLastName}
+                                            </Card.Text>
+                                        </Col>
+                                    </Row>
+                                    <Row className="mb-2">
+                                        <Col xs={4}>
+                                            <Card.Text>EPC</Card.Text>
+                                        </Col>
+                                        <Col xs={1}>
+                                            <Card.Text>:</Card.Text>
+                                        </Col>
+                                        <Col xs={7}>
+                                            <Card.Text>{m.epc}</Card.Text>
+                                        </Col>
+                                    </Row>
+                                    <Row className="mb-2">
+                                        <Col xs={4}>
+                                            <Card.Text>Fecha</Card.Text>
+                                        </Col>
+                                        <Col xs={1}>
+                                            <Card.Text>:</Card.Text>
+                                        </Col>
+                                        <Col xs={7}>
+                                            <Card.Text>
+                                                {timestampDate(m.timestamp)} -{' '}
+                                                {timestampTime(m.timestamp)}
+                                            </Card.Text>
+                                        </Col>
+                                    </Row>
                                 </Card.Body>
                             </Card>
-                            // <Badge
-                            //     pill
-                            //     bg="secondary"
-                            //     key={m.id}
-                            //     className="me-2"
-                            // >
-                            //     {m.productId}
-                            // </Badge>
                         ))}
-                    </div>
+                    </Col>
                 </ListGroup.Item>
 
                 {/* Sección de Alertas de Stock Bajo */}
