2f41a575be7aa3c8f3ae60023d84678ac2fcf1f0
DevKam <camilo.santander1991@gmail.com>
2023-11-20 02:09:38 -0300

    Add formik form and API call to get last added products

--- src/modules/dashboard/components/inventory/InVolumeInfo.jsx
+++ src/modules/dashboard/components/inventory/InVolumeInfo.jsx
@@ -1,43 +1,94 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button, Col, Form, InputGroup, Row } from 'react-bootstrap';
 import { CustomBarChart } from '../../../../shared/ui/components/charts/CustomBarChart';
+import { useFormik } from 'formik';
+import { getLastAddedProducts } from '../../../movements/APIs/movementAPI';
 
 export const InVolumeInfo = () => {
+    const [entriesData, setEntriesData] = useState([]);
+
+    const getChartData = async () => {
+        try {
+            const { data } = await getLastAddedProducts();
+
+            const totalsByWarehouse = data.reduce((acc, item) => {
+                // Si el warehouseName ya existe en el acumulador, incrementa el contador
+                if (acc[item.warehouseName]) {
+                    acc[item.warehouseName]++;
+                } else {
+                    // Si no, inicializa el contador para ese warehouseName
+                    acc[item.warehouseName] = 1;
+                }
+                return acc;
+            }, {});
+
+            const formattedData = Object.entries(totalsByWarehouse).map(
+                ([name, Cantidad]) => {
+                    return { name, Cantidad };
+                },
+            );
+
+            setEntriesData(formattedData);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        getChartData();
+    }, []);
+
+    const handleSubmit = async (values) => {
+        const { startDate, endDate } = values;
+        const resp = await getLastAddedProducts(undefined, startDate, endDate);
+        console.log(resp);
+    };
+
+    const formik = useFormik({
+        initialValues: {
+            startDate: '',
+            endDate: '',
+        },
+        onSubmit: handleSubmit,
+    });
+
     return (
         <>
             <Row>
                 <Col>Entrada</Col>
             </Row>
-            <Row>
-                <Col xs={5}>
-                    <InputGroup size="sm">
-                        <InputGroup.Text>Inicio</InputGroup.Text>
-                        <Form.Control
-                            placeholder="Inicio"
-                            type="date"
-                            name="startDate"
-                            value={formik.values.startDate}
-                            onChange={formik.handleChange}
-                        />
-                    </InputGroup>
-                </Col>
-                <Col xs={5}>
-                    <InputGroup size="sm">
-                        <InputGroup.Text>Termino</InputGroup.Text>
-                        <Form.Control
-                            type="date"
-                            name="endDate"
-                            value={formik.values.endDate}
-                            onChange={formik.handleChange}
-                        />
-                    </InputGroup>
-                </Col>
-                <Col xs={2} className="text-end">
-                    <Button className="col-12" size="sm">
-                        Filtrar
-                    </Button>
-                </Col>
-            </Row>
+            <Form onSubmit={formik.handleSubmit}>
+                <Row>
+                    <Col xs={5}>
+                        <InputGroup size="sm">
+                            <InputGroup.Text>Inicio</InputGroup.Text>
+                            <Form.Control
+                                placeholder="Inicio"
+                                type="date"
+                                name="startDate"
+                                value={formik.values.startDate}
+                                onChange={formik.handleChange}
+                            />
+                        </InputGroup>
+                    </Col>
+                    <Col xs={5}>
+                        <InputGroup size="sm">
+                            <InputGroup.Text>Termino</InputGroup.Text>
+                            <Form.Control
+                                type="date"
+                                name="endDate"
+                                value={formik.values.endDate}
+                                onChange={formik.handleChange}
+                            />
+                        </InputGroup>
+                    </Col>
+                    <Col xs={2} className="text-end">
+                        <Button className="col-12" size="sm" type="submit">
+                            Filtrar
+                        </Button>
+                    </Col>
+                </Row>
+            </Form>
             <Row>
                 <Col>
                     <CustomBarChart
--- src/modules/dashboard/components/inventory/cards/InOutVolumeCard.jsx
+++ src/modules/dashboard/components/inventory/cards/InOutVolumeCard.jsx
@@ -1,126 +1,20 @@
-import React, { useEffect, useState } from 'react';
-import {
-    Button,
-    Card,
-    Col,
-    Form,
-    InputGroup,
-    ListGroup,
-    Row,
-} from 'react-bootstrap';
-import { CustomBarChart } from '../../../../../shared/ui/components/charts/CustomBarChart';
-import { getLastAddedProducts } from '../../../../movements/APIs/movementAPI';
-import { useFormik } from 'formik';
+import React from 'react';
+import { Card, Col, ListGroup, Row } from 'react-bootstrap';
+
 import { InVolumeInfo } from '../InVolumeInfo';
+import { OutVolumeInfo } from '../OutVolumeInfo';
 
 export const InOutVolumeCard = () => {
-    const [entriesData, setEntriesData] = useState([]);
-
-    const getChartData = async () => {
-        try {
-            const { data } = await getLastAddedProducts();
-
-            console.log(data);
-
-            const totalsByWarehouse = data.reduce((acc, item) => {
-                // Si el warehouseName ya existe en el acumulador, incrementa el contador
-                if (acc[item.warehouseName]) {
-                    acc[item.warehouseName]++;
-                } else {
-                    // Si no, inicializa el contador para ese warehouseName
-                    acc[item.warehouseName] = 1;
-                }
-                return acc;
-            }, {});
-
-            const formattedData = Object.entries(totalsByWarehouse).map(
-                ([name, Cantidad]) => {
-                    return { name, Cantidad };
-                },
-            );
-
-            setEntriesData(formattedData);
-
-            console.log(formattedData);
-
-            // const formattedData = {
-            //     name: 'Entrada',
-            //     Cantidad: parseInt(data.qty),
-            // };
-        } catch (error) {
-            console.log(error);
-        }
-    };
-
-    useEffect(() => {
-        getChartData();
-    }, []);
-
-    const formik = useFormik({
-        initialValues: {
-            startDate: '',
-            endDate: '',
-        },
-        onSubmit: (values) => {
-            console.log(values);
-        },
-    });
-
     return (
         <Card>
             <Card.Header>Volumen de Entrada y Salida</Card.Header>
             <Card.Body>
-                <ListGroup horizontal="lg">
-                    <ListGroup.Item className="col-lg-6">
+                <ListGroup horizontal="xxl">
+                    <ListGroup.Item className="col-xxl-6">
                         <InVolumeInfo />
-                        <Row>
-                            <Col>Entrada</Col>
-                        </Row>
-                        <Row>
-                            <Col xs={5}>
-                                <InputGroup size="sm">
-                                    <InputGroup.Text>Inicio</InputGroup.Text>
-                                    <Form.Control
-                                        placeholder="Inicio"
-                                        type="date"
-                                        name="startDate"
-                                        value={formik.values.startDate}
-                                        onChange={formik.handleChange}
-                                    />
-                                </InputGroup>
-                            </Col>
-                            <Col xs={5}>
-                                <InputGroup size="sm">
-                                    <InputGroup.Text>Termino</InputGroup.Text>
-                                    <Form.Control
-                                        type="date"
-                                        name="endDate"
-                                        value={formik.values.endDate}
-                                        onChange={formik.handleChange}
-                                    />
-                                </InputGroup>
-                            </Col>
-                            <Col xs={2} className="text-end">
-                                <Button className="col-12" size="sm">
-                                    Filtrar
-                                </Button>
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col>
-                                <CustomBarChart
-                                    data={entriesData}
-                                    xKey="name"
-                                    yKey="Cantidad"
-                                    barFill="#8884d8"
-                                />
-                            </Col>
-                        </Row>
                     </ListGroup.Item>
-                    <ListGroup.Item className="col-lg-6">
-                        <Row>
-                            <Col>Salida</Col>
-                        </Row>
+                    <ListGroup.Item className="col-xxl-6">
+                        <OutVolumeInfo />
                     </ListGroup.Item>
                 </ListGroup>
             </Card.Body>
